84c034517ca6aa3a929ebc72a78d236571e27b0f
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-28 21:06:20 +0800

    added route handlers

--- src/lib.js
+++ src/lib.js
@@ -16,7 +16,7 @@ module.exports = (() => {
     );
   };
 
-  const app = (port) => {
+  const app = (port, serverInit) => {
     const middleware = (middlewareHandler) => {
       if (typeof middleware == "function") middlewares.push(middlewareHandler);
     };
@@ -32,6 +32,54 @@ module.exports = (() => {
     const del = (route, options = {}, handler) =>
       insertRoute("delete", route, handler, options);
 
+    const listen = (port, cb) => {
+      http
+        .createServer((req, res) => {
+          // Handling middlewares
+          handleMiddlewares(req, res, (err) => {
+            if (err) {
+              res.writeHead(500);
+              res.end("Something went wrong");
+            }
+          });
+
+          function handleMiddlewares(req, res, callback) {
+            let index = 0;
+
+            const next = (err) => {
+              if (err != null) {
+                return setImmediate(() => callback(err));
+              }
+              if (index >= middlewares.length) {
+                return setImmediate(() => callback());
+              }
+
+              const layer = middlewares[index++];
+              setImmediate(() => {
+                try {
+                  layer(req, res, next);
+                } catch (error) {
+                  next(error);
+                }
+              });
+            };
+            next();
+          }
+
+          // Handling routes
+        })
+        .listen(port, (e) => {
+          if (e) return console.log(e);
+          console.log(`${new Date().toUTCString()}`);
+          console.log(`Server started running at port ${port}`);
+          cb && cb();
+        });
+    };
+
+    if (port) {
+      listen(port, serverInit);
+    }
+
     return {
       middleware,
       get,
@@ -39,6 +87,7 @@ module.exports = (() => {
       put,
       patch,
       delete: del,
+      listen,
     };
   };
 
