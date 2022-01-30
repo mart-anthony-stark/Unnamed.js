53f65920832c3bfa768ff322a8d18dc4d3cb7728
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-30 22:17:02 +0800

    added user route endpoints

--- user.route.js
+++ user.route.js
@@ -1,6 +1,18 @@
 const userRouter = (route) => {
-  route.get((req, res) => {
-    res.send({ data: "this is a custom router" });
+  route.get("/", (req, res) => {
+    res.send({ data: "this is a user router" });
+  });
+  route.post("/", (req, res) => {
+    res.send({ data: "this is a user router" });
+  });
+  route.put("/", (req, res) => {
+    res.send({ data: "this is a user router" });
+  });
+  route.patch("/", (req, res) => {
+    res.send({ data: "this is a user router" });
+  });
+  route.delete("/", (req, res) => {
+    res.send({ data: "this is a user router" });
   });
 };
 module.exports = userRouter;
