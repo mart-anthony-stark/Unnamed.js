fc052017ee2f2cba74e946b276c346dd479e5c74
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-31 19:13:42 +0800

    added response body

--- user.route.js
+++ user.route.js
@@ -1,18 +1,38 @@
 const userRouter = (route) => {
-  route.get("/", (req, res) => {
-    res.send({ data: "this is a user router" });
+  route.get("/name", (req, res) => {
+    res.send({
+      data: "this is a user router",
+      method: req.method,
+      url: req.url,
+    });
   });
   route.post("/", (req, res) => {
-    res.send({ data: "this is a user router" });
+    res.send({
+      data: "this is a user router",
+      method: req.method,
+      url: req.url,
+    });
   });
   route.put("/", (req, res) => {
-    res.send({ data: "this is a user router" });
+    res.send({
+      data: "this is a user router",
+      method: req.method,
+      url: req.url,
+    });
   });
   route.patch("/", (req, res) => {
-    res.send({ data: "this is a user router" });
+    res.send({
+      data: "this is a user router",
+      method: req.method,
+      url: req.url,
+    });
   });
   route.delete("/", (req, res) => {
-    res.send({ data: "this is a user router" });
+    res.send({
+      data: "this is a user router",
+      method: req.method,
+      url: req.url,
+    });
   });
 };
 module.exports = userRouter;
