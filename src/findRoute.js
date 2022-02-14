33db370f4672a417a2dba0a05bd0fcf9f7142f59
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-14 19:38:01 +0800

    add: params parsing

--- src/findRoute.js
+++ src/findRoute.js
@@ -1,18 +1,19 @@
 const Route = require("./route-parser");
 
 const findRoute = (routes, method, url) => {
-  return routes.find((route) => {
-    let rUrl = route.url.toLowerCase();
-    url = url.toLowerCase();
-    if (rUrl.substring(rUrl.length - 1) === "/" && rUrl.length !== 1) {
-      rUrl = rUrl.slice(0, rUrl.length - 1);
-    }
-    if (url.substring(url.length - 1) === "/" && url.length !== 1) {
-      url = url.slice(0, url.length - 1);
-    }
-    console.log({ url, rUrl });
-    return rUrl === url && route.method.toLowerCase() === method.toLowerCase();
+  const route = routes.find((route) => {
+    return route.method === method && route.url.match(url);
   });
+
+  if (!route) return null;
+  return { handler: route.handler, params: route.url.match(url) };
 };
 
 module.exports = findRoute;
+
+// if (rUrl.substring(rUrl.length - 1) === "/" && rUrl.length !== 1) {
+//   rUrl = rUrl.slice(0, rUrl.length - 1);
+// }
+// if (url.substring(url.length - 1) === "/" && url.length !== 1) {
+//   url = url.slice(0, url.length - 1);
+// }
