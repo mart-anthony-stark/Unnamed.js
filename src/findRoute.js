6a1f2ce696210b7ed36de2eaa6d3aeca516e0e98
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-14 19:45:09 +0800

    add: delete extra slash to end of the entered url

--- src/findRoute.js
+++ src/findRoute.js
@@ -1,6 +1,10 @@
 const Route = require("./route-parser");
 
 const findRoute = (routes, method, url) => {
+  // delete extra slash to end of the entered url
+  if (url.substring(url.length - 1) === "/" && url.length !== 1) {
+    url = url.slice(0, url.length - 1);
+  }
   const route = routes.find((route) => {
     return route.method === method && route.url.match(url);
   });
@@ -10,10 +14,3 @@ const findRoute = (routes, method, url) => {
 };
 
 module.exports = findRoute;
-
-// if (rUrl.substring(rUrl.length - 1) === "/" && rUrl.length !== 1) {
-//   rUrl = rUrl.slice(0, rUrl.length - 1);
-// }
-// if (url.substring(url.length - 1) === "/" && url.length !== 1) {
-//   url = url.slice(0, url.length - 1);
-// }
