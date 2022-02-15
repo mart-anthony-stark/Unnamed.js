7fdd4353aa31f2336b818b888f8bb25738750f16
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-15 19:32:19 +0800

    mod: server name

--- app.js
+++ app.js
@@ -1,52 +1,52 @@
 const unnamed = require("./src");
 
-const app = unnamed({
+const server = unnamed({
   port: 3000,
   init: () => {
     console.log("App is running");
   },
 });
 
-app.middleware((req, res, done) => {
+server.middleware((req, res, done) => {
   console.log("This is middleware 1");
   req.user = "mart";
   done();
 });
 
-app.middleware((req, res, done) => {
+server.middleware((req, res, done) => {
   console.log("This is middleware 2");
   console.log(req.user);
   done();
 });
 
-app.registerRouter({
+server.registerRouter({
   prefix: "user",
   router: require("./user.route"),
 });
 
-app.get("/", (req, res) => {
+server.get("/", (req, res) => {
   console.log(req.user);
   console.log({ name: req.query.name });
   res.code(401).send({ data: { user: req.user }, error: "Unauthorized" });
 });
 
-app.get("/:id", (req, res) => {
+server.get("/:id", (req, res) => {
   console.log(req.params);
   res.code(200).send({ id: req.params.id });
 });
 
-app.post("/", (req, res) => {
+server.post("/", (req, res) => {
   res.code(200).send({ Created: req.body });
 });
 
-app.put("/", (req, res) => {
+server.put("/", (req, res) => {
   res.send({ msg: "Modified" });
 });
 
-app.patch("/", (req, res) => {
+server.patch("/", (req, res) => {
   res.code(200).send({ msg: "Updated" });
 });
 
-app.delete("/", (req, res) => {
+server.delete("/", (req, res) => {
   res.code(200).send("Ok");
 });
