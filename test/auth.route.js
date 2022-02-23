ad5a5a4fc41cc44d701ec60a7b4454ec62d668c2
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-23 22:21:15 +0800

    delete unnecessary files

--- test/auth.route.js
+++
@@ -1,8 +0,0 @@
-module.exports = (router) => {
-  router.GET("/", (req, res) => {
-    res.send("Auth");
-  });
-  router.GET("/:id", (req, res) => {
-    res.send({ id: req.params.id });
-  });
-};
