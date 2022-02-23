a7b1d4ea707235165f6525a592b13df9d402ef72
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-23 19:36:16 +0800

    add: middleware

--- test/index.js
+++ test/index.js
@@ -4,11 +4,16 @@ const server = unnamed({
   port: 3000,
 });
 
+server.middleware((req, res) => {
+  console.log("middleware");
+  req.user = "Mart";
+});
+
 server.registerRouter({
   prefix: "auth",
   router: require("./auth.route"),
 });
 
 server.GET("/user", (req, res) => {
-  res.send("Hello");
+  res.send("Hello" + req.user);
 });
