c9e085ab610707e20c6c0d277f34f106028b22a8
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-09 11:09:23 +0800

    add logger

--- demoV2/app.js
+++ demoV2/app.js
@@ -1,29 +1,32 @@
 const unnamed = require("unnamed-js");
-const mongoose = require("mongoose");
-const cors = require("cors");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
 
 // env variables
-require("dotenv").config({});
+// require("dotenv").config({});
 
 const server = unnamed({
   port: process.env.PORT || 5000,
   init: async () => {
     try {
-      mongoose.connect(process.env.DB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      console.log("Connected to database");
+      // mongoose.connect(process.env.DB_URI, {
+      //   useNewUrlParser: true,
+      //   useUnifiedTopology: true,
+      // });
+      // console.log("Connected to database");
     } catch (error) {
       console.log(error);
     }
   },
 });
 
-server.middleware(cors("*"));
+// server.middleware(cors("*"));
 
-server.combineRouters(require("./routes"));
+// server.combineRouters(require("./routes"));
 
+const morgan = require("morgan");
+server.middleware(morgan("tiny"));
 server.GET("/", (req, res) => {
   res.send("hello");
+  console.log("hello");
 });
